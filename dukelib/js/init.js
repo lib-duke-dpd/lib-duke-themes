e7c8022baf89e8c092c8aeb183c68635e06f8bda
derrekc <dlc32@duke.edu>
2012-08-16 11:27:35 -0400

    makes use of try-catch when detecting presence of jCarouselLite

--- dukelib/js/init.js
+++ dukelib/js/init.js
@@ -1,17 +1,20 @@
 (function($) {
 	Drupal.behaviors.dukelib = {
 		attach: function(context, settings) {
-			$('.item-list', '.myCarouselNews').jCarouselLite({
-				btnNext		: '#nextNews',
-				btnPrev		: '#prevNews',
-				visible		: 3,
-				scroll		: 1,
-				easing		: 'easeInOut',
-				auto		: 4340,
-				speed		: 1000,
-				circular	: true,
-				vertical	: false
-			});
+			try {
+				$('.item-list', '.myCarouselNews').jCarouselLite({
+					btnNext		: '#nextNews',
+					btnPrev		: '#prevNews',
+					visible		: 3,
+					scroll		: 1,
+					easing		: 'easeInOut',
+					auto		: 4340,
+					speed		: 1000,
+					circular	: true,
+					vertical	: false
+				});
+			} catch(err) {
+			}
 		}
 	}
 })(jQuery);
