30689562e667ee3ceeadda6f979cc92c9cdd33a9
derrekc <dlc32@duke.edu>
2012-10-18 12:01:34 -0400

    added code to init the exhibit widget

--- data_and_gis/js/init-exhibit.js
+++ data_and_gis/js/init-exhibit.js
@@ -1,7 +1,22 @@
 (function($) {
 	Drupal.behaviors.data_and_gis = {
 		attach : function(context, settings) {
-			alert(SimileAjax);
+			var fDone = function() {
+				var params = SimileAjax.parseURLParameters();
+				var options = ["keywords", "format", "subject" , "label"];
+					for ( var i in options ) {
+						if (option in params) {
+							if (params[option]) {
+								$('#' + option + '-facet').attr('ex:selection', params[option]);
+							} else {
+								$('#' + option + '-facet').attr('ex:selectMissing', 'true');
+							}
+						}
+					}
+					window.exhibit = Exhibit.create();
+					window.exhibit.configureFromDOM();
 		}
+		window.database = Exhibit.Database.create();
+		window.database.loadDataLinks(fDone);
 	}
 })(jQuery);
