e69366a7925c785fb96540f256d6796323cdb005
derrekc <dlc32@duke.edu>
2012-10-19 10:22:05 -0400

    no msg

--- data_and_gis/templates/page--collections-page.tpl.php
+++ data_and_gis/templates/page--collections-page.tpl.php
@@ -157,7 +157,21 @@
       <?php if ($action_links): ?>
         <ul class="action-links"><?php print render($action_links); ?></ul>
       <?php endif; ?>
+	  <div>
+		<p><a class="masterToggle" href="#">Expand/collapse all &#187;</a></p>
+
+		<div ex:role="facet" ex:facetClass="AlphaRangeFacet" queryParamName="alpha" ex:facetLabel="Alphabetical" ex:expression=".label" ex:interval="3" ex:collapsible="true" ></div>
+		<div> 
+		<div expression=".subject" facetLabel="Subject" role="facet" id="subject-facet"></div> 
+		<div expression=".keywords" facetLabel="Keywords" role="facet" id="keywords-facet"></div>  
+		<div expression=".format" facetLabel="Format" role="facet" id="format-facet"></div> 
+		<div class="hidden" expression=".label" facetLabel="Label" role="facet" id="label-facet"></div>
+		</div> 
+	  
+	  </div>
+	  <div>
       <?php print render($page['content']); ?>
+	  </div>
       <?php print $feed_icons; ?>
     </div><!-- /#content -->
 
