44d0f1e56e674befac73007a94ebd7f2cc5b6c4b
derrekc <dlc32@duke.edu>
2012-10-19 11:46:39 -0400

    no msg

--- data_and_gis/template.php
+++ data_and_gis/template.php
@@ -238,6 +238,7 @@ function data_and_gis_preprocess_panels_pane($variables) {
 			// inspect the block object and locate the menu items..
 			// TODO
 			kpr($variables);
+			kpr($current_path);
 		}
 	}
 }
