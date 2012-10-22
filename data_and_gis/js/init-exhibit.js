24097a6099b086b33881dd284ec3450cfa344333
derrekc <dlc32@duke.edu>
2012-10-22 13:43:54 -0400

    uses Drupal attach behavior

--- data_and_gis/js/init-exhibit.js
+++ data_and_gis/js/init-exhibit.js
@@ -1,23 +1,25 @@
- SimileAjax.jQuery(document).ready(function() {
-
- var fDone = function() {
-   var params = SimileAjax.parseURLParameters();
-   var options = ["keywords", "format", "subject","label"];
-	 for ( var i in options ) {
-	   var option = options[i];
-	   if (option in params) {
-		 if (params[option]) {
-		   document.getElementById(option + "-facet").setAttribute("ex:selection", params[option]);
-		 }
-		 else {
-		  document.getElementById(option +
-			"-facet").setAttribute("ex:selectMissing", "true");
-		 }
-	   }
-	 }
-	 window.exhibit = Exhibit.create();
-	 window.exhibit.configureFromDOM();
- };
- window.database = Exhibit.Database.create();
- window.database.loadDataLinks(fDone);
-});
+(function($){
+	Drupal.behaviors.data_and_gis = {
+		attach: function(context, settings) {
+			var fDone = function() {
+				var params = SimileAjax.parseURLParameters();
+				var options = ["keywords", "format", "subject","label"];
+				for ( var i in options ) {
+					var option = options[i];
+					if (option in params) {
+						if (params[option]) {
+							$('#' + option + "-facet").attr("ex:selection", params[option]);
+						}
+						else {
+							$('#' + option + "-facet").attr("ex:selectMissing", "true");
+						}
+					}
+				}
+				window.exhibit = Exhibit.create();
+				window.exhibit.configureFromDOM();
+			};
+			window.database = Exhibit.Database.create();
+			window.database.loadDataLinks(fDone);
+		}
+	}
+})(jQuery);
