96f84a1898a1cc9497d47fe65eea30145d3099dd
derrekc <dlc32@duke.edu>
2012-10-23 15:53:52 -0400

    added jQuery/javascript code to show and hide collection elements

--- data_and_gis/js/init-exhibit.js
+++ data_and_gis/js/init-exhibit.js
@@ -1,6 +1,7 @@
 (function($){
 	Drupal.behaviors.data_and_gis = {
 		attach: function(context, settings) {
+			debugger;
 			var fDone = function() {
 				var params = SimileAjax.parseURLParameters();
 				var options = ["keywords", "format", "subject","label"];
