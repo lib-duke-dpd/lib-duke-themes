48ba321080952f49706663e2ccf2923e80e5796d
derrekc <dlc32@duke.edu>
2012-10-24 13:23:01 -0400

    added the sidebar regions back to the theme

--- data_and_gis/templates/page--collections-page.tpl.php
+++ data_and_gis/templates/page--collections-page.tpl.php
@@ -277,6 +277,19 @@
 
     </div><!-- /#navigation -->
 
+		<?php
+			// Render the sidebars to see if there's anything in them.
+			$sidebar_first = render($page['sidebar_first']);
+			$sidebar_second = render($page['sidebar_second']);
+		?>
+		
+		<?php if ($sidebar_first || $sidebar_second): ?>
+			<aside class="sidebars">
+				<?php print $sidebar_first; ?>
+				<?php print $sidebar_second; ?>
+			</aside>
+		<?php endif; ?>
+
 
   </div><!-- /#main -->
 
